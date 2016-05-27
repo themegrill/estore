79b65e4a93eb78881c433fe8213f59ef233e3aaf
Rajeeb Banstola <rajeebthegreat@gmail.com>
2016-05-27 15:16:45 +0545

    Adds support for WP Page-Navi

--- index.php
+++ index.php
@@ -52,8 +52,6 @@ get_header(); ?>
 
 					   <?php endwhile; ?>
 
-					   <?php the_posts_navigation(); ?>
-
 					<?php else : ?>
 
 					   <?php get_template_part( 'template-parts/content', 'none' ); ?>
---
+++ navigation.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The template part for displaying navigation.
+ *
+ * @package ThemeGrill
+ * @subpackage eStore
+ * @since eStore 1.1
+ */
+?>
+
+<?php
+if( is_archive() || is_home() || is_search() ) {
+	/**
+	 * Checking WP-PageNaviplugin exist
+	 */
+	if ( function_exists('wp_pagenavi' ) ) :
+		wp_pagenavi();
+
+	else:
+		global $wp_query;
+		if ( $wp_query->max_num_pages > 1 ) :
+		?>
+		<ul class="default-wp-page clearfix">
+			<li class="previous"><?php next_posts_link( esc_html__( '&larr; Previous', 'estore' ) ); ?></li>
+			<li class="next"><?php previous_posts_link( esc_html__( 'Next &rarr;', 'estore' ) ); ?></li>
+		</ul>
+		<?php
+		endif;
+	endif;
+}
+
+if ( is_single() ) {
+	if( is_attachment() ) {
+	?>
+		<ul class="default-wp-page clearfix">
+			<li class="previous"><?php previous_image_link( false, esc_html__( '&larr; Previous', 'estore' ) ); ?></li>
+			<li class="next"><?php next_image_link( false, esc_html__( 'Next &rarr;', 'estore' ) ); ?></li>
+		</ul>
+	<?php
+	}
+	else {
+	?>
+		<ul class="default-wp-page clearfix">
+			<li class="previous"><?php previous_post_link( '%link', '<span class="meta-nav">' . esc_html_x( '&larr;', 'Previous post link', 'estore' ) . '</span> %title' ); ?></li>
+			<li class="next"><?php next_post_link( '%link', '%title <span class="meta-nav">' . esc_html_x( '&rarr;', 'Next post link', 'estore' ) . '</span>' ); ?></li>
+		</ul>
+	<?php
+	}
+}
+?>
