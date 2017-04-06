b09d22ea4b2b51747f8806a493f189c2d390ba68
saazaan7 <sajanjoshi0@gmail.com>
2017-04-06 15:10:08 +0545

    Add - Support for WooCommerce 3.0 Product Gallery

--- functions.php
+++ functions.php
@@ -112,6 +112,10 @@ function estore_setup() {
 
 	// Declare WooCommerce Support
 	add_theme_support( 'woocommerce' );
+
+	add_theme_support( 'wc-product-gallery-lightbox' );
+	add_theme_support( 'wc-product-gallery-zoom' );
+	add_theme_support( 'wc-product-gallery-slider' );
 }
 endif; // estore_setup
 add_action( 'after_setup_theme', 'estore_setup' );
