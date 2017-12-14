98d72bd993122b7c01af850afb71d9787eb7dad1
Nima <nima.themegrill@gmail.com>
2017-12-14 14:40:23 +0545

    Code adjustment for add to cart button in shop page

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -21,11 +21,6 @@ remove_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_show_prod
 // Adds our own product-thumbnail to loop
 add_action( 'woocommerce_before_shop_loop_item_title', 'estore_template_loop_product_thumbnail', 11 );
 
-// Removes add-to-cart button from loop
-if ( get_theme_mod( 'estore_woocommerce_product_thumb_mask' ) == 0 ) {
-	remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
-}
-
 // Removes link end
 remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
 
@@ -330,7 +325,11 @@ if ( ! function_exists( 'estore_template_loop_product_thumbnail' ) ) {
 							<?php woocommerce_template_loop_add_to_cart( $product ); ?>
 						</div>
 					</div><!-- featured hover end -->
-				<?php endif; ?>
+				<?php
+						remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+					else:
+						add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+				  endif; ?>
 			</figure>
 		<?php
 		} else { ?>
@@ -347,7 +346,11 @@ if ( ! function_exists( 'estore_template_loop_product_thumbnail' ) ) {
 							<?php woocommerce_template_loop_add_to_cart( $product ); ?>
 						</div>
 					</div><!-- featured hover end -->
-				<?php endif; ?>
+				<?php 
+						remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+					else:
+						add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart', 10 );
+					endif; ?>
 			</figure>
 		<?php }
 	}
