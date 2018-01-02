095e8a4da366e187fa1974a2c1ced14afb126400
Nima <nima.themegrill@gmail.com>
2018-01-02 16:59:54 +0545

    updated template file add-to-cart

--- woocommerce/loop/add-to-cart.php
+++ woocommerce/loop/add-to-cart.php
@@ -20,12 +20,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 global $product;
 echo apply_filters( 'woocommerce_loop_add_to_cart_link',
-	sprintf( '<a rel="nofollow" href="%s" data-quantity="%s" data-product_id="%s" data-product_sku="%s" class="%s">%s<i class="fa fa-shopping-cart"></i></a>',
+	sprintf( '<a href="%s" data-quantity="%s" class="%s" %s>%s<i class="fa fa-shopping-cart"></i></a>',
 		esc_url( $product->add_to_cart_url() ),
-		esc_attr( isset( $quantity ) ? $quantity : 1 ),
-		esc_attr( $product->get_id() ),
-		esc_attr( $product->get_sku() ),
-		esc_attr( isset( $class ) ? $class : 'button' ),
+		esc_attr( isset( $args['quantity'] ) ? $args['quantity'] : 1 ),
+		esc_attr( isset( $args['class'] ) ? $args['class'] : 'button' ),
+		isset( $args['attributes'] ) ? wc_implode_html_attributes( $args['attributes'] ) : '',
 		esc_html( $product->add_to_cart_text() )
 	),
-$product );
+$product, $args );
