76f2cb5552740ca08bba19dfc2fdc4cf5d791e0c
Ashish Shrestha <ashishsthanp@gmail.com>
2018-04-09 11:33:41 +0545

    Custom product search form

--- inc/functions.php
+++ inc/functions.php
@@ -30,3 +30,25 @@ function estore_header_search_box() {
 	</div>
 	<?php
 }
+
+/**
+ * estore_product_searchform
+ * Overrides default WC search form
+ *
+ * @since 1.2.5
+ */
+function estore_product_searchform ( $form ) {
+
+	$form = '<form role="search" method="get" class="estore-wc-product-search" action=" '. esc_url( home_url( '/' ) ) . '">
+		<label class="screen-reader-text" for="estore-wc-search-field">' . esc_html__( 'Search for:', 'estore' ) . '</label>
+		<input type="search" id="estore-wc-search-field" class="search-field" placeholder="'. esc_attr__( 'Search products ...', 'estore' ) . '" value="' . get_search_query() . '" name="s" />
+		<button type="submit" class="searchsubmit" value="' . esc_attr_x( 'Search', 'submit button', 'estore' ) . '">
+			<i class="fa fa-search"></i>
+		</button>
+		<input type="hidden" name="post_type" value="product" />
+	</form>';
+
+	return $form;
+
+}
+add_filter( 'get_product_search_form' , 'estore_product_searchform', 10, 1 );
