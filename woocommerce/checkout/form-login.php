029aa4bea8d30d2f5f410dcf2309d7622f45b241
Rajesh Poudel <razyees@gmail.com>
2018-05-30 16:29:20 +0545

    Fix - text-domain

--- woocommerce/checkout/form-login.php
+++ woocommerce/checkout/form-login.php
@@ -23,13 +23,13 @@ if ( is_user_logged_in() || 'no' === get_option( 'woocommerce_enable_checkout_lo
 ?>
 <div class="login-form-info-wrapper">
 	<?php
-	wc_print_notice( apply_filters( 'woocommerce_checkout_login_message', __( 'Returning customer?', 'woocommerce' ) ) . ' <a href="#" class="showlogin">' . __( 'Click here to login', 'woocommerce' ) . '</a>', 'notice' );
+	wc_print_notice( apply_filters( 'woocommerce_checkout_login_message', __( 'Returning customer?', 'estore' ) ) . ' <a href="#" class="showlogin">' . __( 'Click here to login', 'estore' ) . '</a>', 'notice' );
 	?>
 
 	<?php
 		woocommerce_login_form(
 			array(
-				'message'  => __( 'If you have shopped with us before, please enter your details in the boxes below. If you are a new customer, please proceed to the Billing &amp; Shipping section.', 'woocommerce' ),
+				'message'  => __( 'If you have shopped with us before, please enter your details in the boxes below. If you are a new customer, please proceed to the Billing &amp; Shipping section.', 'estore' ),
 				'redirect' => wc_get_page_permalink( 'checkout' ),
 				'hidden'   => true
 			)
