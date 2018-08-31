ed954c345685ad7fd75ede554027f9a407376999
Nitu Shrestha <shresthanitu984@gmail.com>
2018-08-31 14:32:12 +0545

    Update - PHP DocBlock

--- inc/demo-config.php
+++ inc/demo-config.php
@@ -2,21 +2,18 @@
 /**
  * Functions for configuring demo importer.
  *
- * @author   ThemeGrill
- * @category Admin
- * @package  Importer/Functions
- * @version  1.0.0
+ * @package Importer/Functions
+ * @version 1.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+defined( 'ABSPATH' ) || exit;
 
 /**
- * Setup demo importer packages.
+ * Setup demo importer config.
  *
- * @param  array $packages
+ * @deprecated 1.5.0
  *
+ * @param  array $demo_config Demo config.
  * @return array
  */
 function estore_demo_importer_packages( $packages ) {
