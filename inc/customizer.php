2c1dedc4b76fabe58a17145e1c2fbb7108f27b50
Nilan Bhasima <nilanbhas@gmail.com>
2018-09-04 10:06:08 +0545

    added styles for view-pro tab in customizer

--- inc/customizer.php
+++ inc/customizer.php
@@ -970,17 +970,42 @@ add_action( 'customize_controls_print_footer_scripts', 'estore_customizer_custom
 function estore_customizer_custom_scripts() { ?>
 	<style>
 		/* Theme Instructions Panel CSS */
-		li#accordion-section-estore_important_links h3.accordion-section-title, li#accordion-section-estore_important_links h3.accordion-section-title:focus {
+		li#accordion-section-estore_upsell_section h3.accordion-section-title {
 			background-color: #289DCC !important;
+			border-left-color: #0073aa !important;
+		}
+
+		#accordion-section-estore_upsell_section h3 a:after {
+			content: '\f345';
+			color: #fff;
+			position: absolute;
+			top: 12px;
+			right: 10px;
+			z-index: 1;
+			font: 400 20px/1 dashicons;
+			speak: none;
+			display: block;
+			-webkit-font-smoothing: antialiased;
+			-moz-osx-font-smoothing: grayscale;
+			text-decoration: none!important;
+		}
+
+		li#accordion-section-estore_upsell_section h3.accordion-section-title a {
+			display: block;
+			color: #fff !important;
+			text-decoration: none;
+		}
+
+		li#accordion-section-estore_upsell_section h3.accordion-section-title a:focus {
+			box-shadow: none;
+		}
+
+		li#accordion-section-estore_upsell_section h3.accordion-section-title:hover {
+			background-color: #1b90bf !important;
 			color: #fff !important;
 		}
 
-		li#accordion-section-estore_important_links h3.accordion-section-title:hover {
-			background-color: #289DCC !important;
-			color: #fff !important;
-		}
-
-		li#accordion-section-estore_important_links h3.accordion-section-title:after {
+		li#accordion-section-estore_upsell_section h3.accordion-section-title:after {
 			color: #fff !important;
 		}
 
