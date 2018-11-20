c80ef8c3d0940c760f318206f7346359d4dccb99
razyees <rajeshpoudel9849@gmail.com>
2018-11-20 14:14:04 +0545

    Add - Alt text for post image

--- inc/widgets/class-estore-featured-posts-widget.php
+++ inc/widgets/class-estore-featured-posts-widget.php
@@ -114,8 +114,16 @@ class estore_featured_posts_widget extends WP_Widget {
 								?>
 
 								<?php
-								if( has_post_thumbnail() ) {
-									the_post_thumbnail('estore-featured-image');
+								if ( has_post_thumbnail() ) {
+									$title_attribute     = esc_attr( get_the_title( $post->ID ) );
+									$thumb_id            = get_post_thumbnail_id( get_the_ID() );
+									$img_altr            = get_post_meta( $thumb_id, '_wp_attachment_image_alt', true );
+									$img_alt             = ! empty( $img_altr ) ? $img_altr : $title_attribute;
+									$post_thumbnail_attr = array(
+										'alt'   => esc_attr( $img_alt ),
+										'title' => esc_attr( $title_attribute ),
+									);
+									the_post_thumbnail('estore-featured-image', $post_thumbnail_attr);
 								} else { ?>
 									<img src='<?php echo get_template_directory_uri(); ?>/images/placeholder-blog-380x250.jpg' alt='<?php esc_attr__('Blog Image', 'estore');?>' width="380" height="250" />
 								<?php } ?>
