6b08a534589215799eac2135a41d22da1db076bf
ankitkarki17 <ankitkarki17@gmail.com>
2020-08-12 18:14:46 +0545

    Refactor code

--- inc/admin/class-estore-welcome-notice.php
+++ inc/admin/class-estore-welcome-notice.php
@@ -8,7 +8,7 @@
  */
 
 // Exit if accessed directly.
-defined('ABSPATH') || exit;
+defined( 'ABSPATH' ) || exit;
 
 /**
  * Class eStore_Welcome_Notice
@@ -20,168 +20,170 @@ class eStore_Welcome_Notice {
 	 *
 	 * eStore_Welcome_Notice constructor.
 	 */
-    public function __construct() {
-        add_action('wp_loaded', array($this, 'welcome_notice'), 20);
-        add_action('wp_loaded', array($this, 'hide_notices'), 15);
-        add_action('wp_ajax_import_button', array($this, 'welcome_notice_import_handler'));
-    }
+	public function __construct() {
+		add_action( 'wp_loaded', array( $this, 'welcome_notice' ), 20 );
+		add_action( 'wp_loaded', array( $this, 'hide_notices' ), 15 );
+		add_action( 'wp_ajax_import_button', array( $this, 'welcome_notice_import_handler' ) );
+	}
 
-    public function welcome_notice() {
-        if (!get_option('estore_admin_notice_welcome')) {
-            add_action('admin_notices', array($this, 'welcome_notice_markup')); // Show notice.
-        }
-    }
+	public function welcome_notice() {
+		if ( ! get_option( 'estore_admin_notice_welcome' ) ) {
+			add_action( 'admin_notices', array( $this, 'welcome_notice_markup' ) ); // Show notice.
+		}
+	}
 
-    /**
-     * echo `Get started` CTA.
-     *
-     * @return string
-     *
-     */
-    public function import_button_html() {
-        $html = '<a class="btn-get-started button button-primary button-hero" href="#" data-name="' . esc_attr('themegrill-demo-importer') . '" data-slug="' . esc_attr('themegrill-demo-importer') . '" aria-label="' . esc_attr__('Get started with eStore', 'estore') . '">' . esc_html__('Get started with eStore', 'estore') . '</a>';
+	/**
+	 * echo `Get started` CTA.
+	 *
+	 * @return string
+	 *
+	 */
+	public function import_button_html() {
+		$html = '<a class="btn-get-started button button-primary button-hero" href="#" data-name="' . esc_attr( 'themegrill-demo-importer' ) . '" data-slug="' . esc_attr( 'themegrill-demo-importer' ) . '" aria-label="' . esc_attr__( 'Get started with eStore', 'estore' ) . '">' . esc_html__( 'Get started with eStore', 'estore' ) . '</a>';
 
-        return $html;
-    }
+		return $html;
+	}
 
-    /**
-     * Show welcome notice.
-     */
-    public function welcome_notice_markup() {
-        $dismiss_url = wp_nonce_url(
-            remove_query_arg(array('activated'), add_query_arg('estore-hide-notice', 'welcome')),
-            'estore_hide_notices_nonce',
-            '_estore_notice_nonce'
-        );
-?>
-        <div id="message" class="notice notice-success estore-notice">
-            <a class="estore-message-close notice-dismiss" href="<?php echo esc_url($dismiss_url); ?>"></a>
+	/**
+	 * Show welcome notice.
+	 */
+	public function welcome_notice_markup() {
+		$dismiss_url = wp_nonce_url(
+			remove_query_arg( array( 'activated' ), add_query_arg( 'estore-hide-notice', 'welcome' ) ),
+			'estore_hide_notices_nonce',
+			'_estore_notice_nonce'
+		);
+		?>
+		<div id="message" class="notice notice-success estore-notice">
+			<a class="estore-message-close notice-dismiss" href="<?php echo esc_url( $dismiss_url ); ?>"></a>
 
-            <div class="estore-message__content">
-                <div class="estore-message__image">
-                    <img class="estore-screenshot" src="<?php echo get_template_directory_uri(); ?>/screenshot.jpg" alt="<?php esc_html_e('eStore', 'estore'); ?>" /><?php // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped, Squiz.PHP.EmbeddedPhp.SpacingBeforeClose
-                                                                                                                                                                            ?>
-                </div>
+			<div class="estore-message__content">
+				<div class="estore-message__image">
+					<img class="estore-screenshot" src="<?php echo get_template_directory_uri(); ?>/screenshot.jpg" alt="<?php esc_html_e( 'eStore', 'estore' ); ?>" />
+																   <?php
+                    // phpcs:ignore WordPress.XSS.EscapeOutput.OutputNotEscaped, Squiz.PHP.EmbeddedPhp.SpacingBeforeClose
+																	?>
+				</div>
 
-                <div class="estore-message__text">
-                    <h2 class="estore-message__heading">
-                        <?php
-                        printf(
-                            /* translators: 1: welcome page link starting html tag, 2: welcome page link ending html tag. */
-                            esc_html__('Welcome! Thank you for choosing estore! To fully take advantage of the best our theme can offer please make sure you visit our %1$swelcome page%2$s.', 'estore'),
-                            '<a href="' . esc_url(admin_url('themes.php?page=estore-welcome')) . '">',
-                            '</a>'
-                        );
-                        ?>
-                    </h2>
+				<div class="estore-message__text">
+					<h2 class="estore-message__heading">
+						<?php
+						printf(
+							/* translators: 1: welcome page link starting html tag, 2: welcome page link ending html tag. */
+							esc_html__( 'Welcome! Thank you for choosing estore! To fully take advantage of the best our theme can offer please make sure you visit our %1$swelcome page%2$s.', 'estore' ),
+							'<a href="' . esc_url( admin_url( 'themes.php?page=estore-welcome' ) ) . '">',
+							'</a>'
+						);
+						?>
+					</h2>
 
-                    <div class="estore-message__cta">
-                        <?php echo $this->import_button_html(); ?>
-                        <span class="plugin-install-notice"><?php esc_html_e('Clicking the button will install and activate the ThemeGrill demo importer plugin.', 'estore'); ?></span>
-                    </div>
-                </div>
-            </div>
-        </div> <!-- /.estore-message__content -->
-<?php
-    }
+					<div class="estore-message__cta">
+						<?php echo $this->import_button_html(); ?>
+						<span class="plugin-install-notice"><?php esc_html_e( 'Clicking the button will install and activate the ThemeGrill demo importer plugin.', 'estore' ); ?></span>
+					</div>
+				</div>
+			</div>
+		</div> <!-- /.estore-message__content -->
+		<?php
+	}
 
-    /**
-     * Hide a notice if the GET variable is set.
-     */
-    public function hide_notices() {
-        if (isset($_GET['estore-hide-notice']) && isset($_GET['_estore_notice_nonce'])) { // WPCS: input var ok.
-            if (!wp_verify_nonce(wp_unslash($_GET['_estore_notice_nonce']), 'estore_hide_notices_nonce')) { // phpcs:ignore WordPress.VIP.ValidatedSanitizedInput.InputNotSanitized
-                wp_die(__('Action failed. Please refresh the page and retry.', 'estore')); // WPCS: xss ok.
-            }
+	/**
+	 * Hide a notice if the GET variable is set.
+	 */
+	public function hide_notices() {
+		if ( isset( $_GET['estore-hide-notice'] ) && isset( $_GET['_estore_notice_nonce'] ) ) { // WPCS: input var ok.
+			if ( ! wp_verify_nonce( wp_unslash( $_GET['_estore_notice_nonce'] ), 'estore_hide_notices_nonce' ) ) { // phpcs:ignore WordPress.VIP.ValidatedSanitizedInput.InputNotSanitized
+				wp_die( __( 'Action failed. Please refresh the page and retry.', 'estore' ) ); // WPCS: xss ok.
+			}
 
-            if (!current_user_can('manage_options')) {
-                wp_die(__('Cheatin&#8217; huh?', 'estore')); // WPCS: xss ok.
-            }
+			if ( ! current_user_can( 'manage_options' ) ) {
+				wp_die( __( 'Cheatin&#8217; huh?', 'estore' ) ); // WPCS: xss ok.
+			}
 
-            $hide_notice = sanitize_text_field(wp_unslash($_GET['estore-hide-notice']));
+			$hide_notice = sanitize_text_field( wp_unslash( $_GET['estore-hide-notice'] ) );
 
-            // Hide.
-            if ('welcome' === $_GET['estore-hide-notice']) {
-                update_option('estore_admin_notice_' . $hide_notice, 1);
-            } else { // Show.
-                delete_option('estore_admin_notice_' . $hide_notice);
-            }
-        }
-    }
+			// Hide.
+			if ( 'welcome' === $_GET['estore-hide-notice'] ) {
+				update_option( 'estore_admin_notice_' . $hide_notice, 1 );
+			} else { // Show.
+				delete_option( 'estore_admin_notice_' . $hide_notice );
+			}
+		}
+	}
 
-    /**
-     * Handle the AJAX process while import or get started button clicked.
-     */
-    public function welcome_notice_import_handler() {
-        check_ajax_referer('estore_demo_import_nonce', 'security');
+	/**
+	 * Handle the AJAX process while import or get started button clicked.
+	 */
+	public function welcome_notice_import_handler() {
+		check_ajax_referer( 'estore_demo_import_nonce', 'security' );
 
-        $state = '';
+		$state = '';
 
-        if (class_exists('themegrill_demo_importer')) {
-            $state = 'activated';
-        } elseif (file_exists(WP_PLUGIN_DIR . '/themegrill-demo-importer/themegrill-demo-importer.php')) {
-            $state = 'installed';
-        }
+		if ( class_exists( 'themegrill_demo_importer' ) ) {
+			$state = 'activated';
+		} elseif ( file_exists( WP_PLUGIN_DIR . '/themegrill-demo-importer/themegrill-demo-importer.php' ) ) {
+			$state = 'installed';
+		}
 
-        if ('activated' === $state) {
-            $response['redirect'] = admin_url('/themes.php?page=demo-importer&browse=all&estore-hide-notice=welcome');
-        } elseif ('installed' === $state) {
-            $response['redirect'] = admin_url('/themes.php?page=demo-importer&browse=all&estore-hide-notice=welcome');
-            if (current_user_can('activate_plugin')) {
-                $result = activate_plugin('themegrill-demo-importer/themegrill-demo-importer.php');
+		if ( 'activated' === $state ) {
+			$response['redirect'] = admin_url( '/themes.php?page=demo-importer&browse=all&estore-hide-notice=welcome' );
+		} elseif ( 'installed' === $state ) {
+			$response['redirect'] = admin_url( '/themes.php?page=demo-importer&browse=all&estore-hide-notice=welcome' );
+			if ( current_user_can( 'activate_plugin' ) ) {
+				$result = activate_plugin( 'themegrill-demo-importer/themegrill-demo-importer.php' );
 
-                if (is_wp_error($result)) {
-                    $response['errorCode']    = $result->get_error_code();
-                    $response['errorMessage'] = $result->get_error_message();
-                }
-            }
-        } else {
-            wp_enqueue_style('plugin-install');
-            wp_enqueue_script('plugin-install');
+				if ( is_wp_error( $result ) ) {
+					$response['errorCode']    = $result->get_error_code();
+					$response['errorMessage'] = $result->get_error_message();
+				}
+			}
+		} else {
+			wp_enqueue_style( 'plugin-install' );
+			wp_enqueue_script( 'plugin-install' );
 
-            $response['redirect'] = admin_url('/themes.php?page=demo-importer&browse=all&estore-hide-notice=welcome');
+			$response['redirect'] = admin_url( '/themes.php?page=demo-importer&browse=all&estore-hide-notice=welcome' );
 
-            /**
-             * Install Plugin.
-             */
-            include_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
-            include_once ABSPATH . 'wp-admin/includes/plugin-install.php';
+			/**
+			 * Install Plugin.
+			 */
+			include_once ABSPATH . 'wp-admin/includes/class-wp-upgrader.php';
+			include_once ABSPATH . 'wp-admin/includes/plugin-install.php';
 
-            $api = plugins_api(
-                'plugin_information',
-                array(
-                    'slug'   => sanitize_key(wp_unslash('themegrill-demo-importer')),
-                    'fields' => array(
-                        'sections' => false,
-                    ),
-                )
-            );
+			$api = plugins_api(
+				'plugin_information',
+				array(
+					'slug'   => sanitize_key( wp_unslash( 'themegrill-demo-importer' ) ),
+					'fields' => array(
+						'sections' => false,
+					),
+				)
+			);
 
-            $skin     = new WP_Ajax_Upgrader_Skin();
-            $upgrader = new Plugin_Upgrader($skin);
-            $result   = $upgrader->install($api->download_link);
+			$skin     = new WP_Ajax_Upgrader_Skin();
+			$upgrader = new Plugin_Upgrader( $skin );
+			$result   = $upgrader->install( $api->download_link );
 
-            if ($result) {
-                $response['installed'] = 'succeed';
-            } else {
-                $response['installed'] = 'failed';
-            }
+			if ( $result ) {
+				$response['installed'] = 'succeed';
+			} else {
+				$response['installed'] = 'failed';
+			}
 
-            // Activate plugin.
-            if (current_user_can('activate_plugin')) {
-                $result = activate_plugin('themegrill-demo-importer/themegrill-demo-importer.php');
+			// Activate plugin.
+			if ( current_user_can( 'activate_plugin' ) ) {
+				$result = activate_plugin( 'themegrill-demo-importer/themegrill-demo-importer.php' );
 
-                if (is_wp_error($result)) {
-                    $response['errorCode']    = $result->get_error_code();
-                    $response['errorMessage'] = $result->get_error_message();
-                }
-            }
-        }
+				if ( is_wp_error( $result ) ) {
+					$response['errorCode']    = $result->get_error_code();
+					$response['errorMessage'] = $result->get_error_message();
+				}
+			}
+		}
 
-        wp_send_json($response);
+		wp_send_json( $response );
 
-        exit();
-    }
+		exit();
+	}
 }
 
 new eStore_Welcome_Notice();
