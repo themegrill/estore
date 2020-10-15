d0e1163f605230c4057d0ee758b4e74dba540127
ankitkarki17 <ankitkarki17@gmail.com>
2020-10-15 22:37:31 +0545

    Fix - Dismiss message notice

--- inc/admin/class-estore-woocommerce-notice.php
+++ inc/admin/class-estore-woocommerce-notice.php
@@ -14,11 +14,10 @@ class eStore_WooCommerce_Notice {
 
 	public function __construct() {
 		add_action( 'wp_loaded', array( $this, 'woocommerce_notice' ), 20 );
-		add_action( 'wp_loaded', array( $this, 'hide_notices' ), 15 );
 	}
 
 	public function woocommerce_notice() {
-		if ( ! get_option( 'estore_admin_notice_welcome' ) && ! class_exists( 'WooCommerce' ) ) {
+		if (  ! class_exists( 'WooCommerce' ) ) {
 			add_action( 'admin_notices', array( $this, 'woocommerce_notice_markup' ) ); // Show notice.
 		}
 	}
@@ -27,17 +26,11 @@ class eStore_WooCommerce_Notice {
 	 * Show WooCommerce notice.
 	 */
 	public function woocommerce_notice_markup() {
-		$dismiss_url = wp_nonce_url(
-			remove_query_arg( array( 'activated' ), add_query_arg( 'estore-hide-notices', 'welcome' ) ),
-			'estore_hide_notices_nonce',
-			'_estore_notice_nonce'
-		);
 
 		$plugin = 'woocommerce/woocommerce.php';
 
 		?>
-		<div id="message" class="notice notice-info estore-notice">
-			<a class="estore-notice-dismiss notice-dismiss" href="<?php echo esc_url( $dismiss_url ); ?>"></a>
+		<div id="message" class="notice notice-info is-dismissible">
 
 			<p><?php esc_html_e( 'If you are planning to create an eCommerce site, install the WooCommerce plugin, otherwise you can dismiss this message notice.', 'estore' ); ?></p>
 
@@ -65,30 +58,6 @@ class eStore_WooCommerce_Notice {
 			?>
 		</div>
 		<?php
-	}
-
-	/**
-	 * Hide a notice if the GET variable is set.
-	 */
-	public function hide_notices() {
-		if ( isset( $_GET['estore-hide-notices'] ) && isset( $_GET['_estore_notice_nonce'] ) ) {
-			if ( ! wp_verify_nonce( wp_unslash( $_GET['_estore_notice_nonce'] ), 'estore_hide_notices_nonce' ) ) { // phpcs:ignore WordPress.VIP.ValidatedSanitizedInput.InputNotSanitized
-				wp_die( __( 'Action failed. Please refresh the page and retry.', 'estore' ) ); // WPCS: xss ok.
-			}
-
-			if ( ! current_user_can( 'manage_options' ) ) {
-				wp_die( __( 'Cheatin&#8217; huh?', 'estore' ) ); // WPCS: xss ok.
-			}
-
-			$hide_notice = sanitize_text_field( wp_unslash( $_GET['estore-hide-notices'] ) );
-
-			// Hide.
-			if ( 'welcome' === $_GET['estore-hide-notices'] ) {
-				update_option( 'estore_admin_notice_' . $hide_notice, 1 );
-			} else { // Show.
-				delete_option( 'estore_admin_notice_' . $hide_notice );
-			}
-		}
 	}
 }
 
