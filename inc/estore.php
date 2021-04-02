bfb2e096dc78074d444aea60a2f0aae70454c247
ankitkarki17 <ankitkarki17@gmail.com>
2021-04-02 15:42:17 +0545

    Reformat code

--- inc/estore.php
+++ inc/estore.php
@@ -96,27 +96,27 @@ if ( ! function_exists( 'estore_breadcrumbs' ) ) :
 	 */
 	function estore_breadcrumbs() {
 		// Start of options.
-		$text['home']     = esc_html__( 'Home', 'estore' ); // text for the 'Home' link
-		$text['category'] = esc_html__( 'Archive by Category "%s"', 'estore' ); // text for a category page
-		$text['tax']      = esc_html__( 'Archive for "%s"', 'estore' ); // text for a taxonomy page
-		$text['search']   = esc_html__( 'Search Results for "%s" query', 'estore' ); // text for a search results page
-		$text['tag']      = esc_html__( 'Posts Tagged "%s"', 'estore' ); // text for a tag page
-		$text['author']   = esc_html__( 'Articles Posted by %s', 'estore' ); // text for an author page
-		$text['404']      = esc_html__( 'Error 404', 'estore' ); // text for the 404 page
-		$text['page']     = esc_html__( 'Page %s', 'estore' ); // text 'Page N'
-		$text['cpage']    = esc_html__( 'Comment Page %s', 'estore' ); // text 'Comment Page N'
+		$text['home']     = esc_html__( 'Home', 'estore' ); // Text for the 'Home' link.
+		$text['category'] = esc_html__( 'Archive by Category "%s"', 'estore' ); // Text for a category page.
+		$text['tax']      = esc_html__( 'Archive for "%s"', 'estore' ); // Text for a taxonomy page.
+		$text['search']   = esc_html__( 'Search Results for "%s" query', 'estore' ); // Text for a search results page.
+		$text['tag']      = esc_html__( 'Posts Tagged "%s"', 'estore' ); // Text for a tag page.
+		$text['author']   = esc_html__( 'Articles Posted by %s', 'estore' ); // Text for an author page.
+		$text['404']      = esc_html__( 'Error 404', 'estore' ); // Text for the 404 page.
+		$text['page']     = esc_html__( 'Page %s', 'estore' ); // Text 'Page N'.
+		$text['cpage']    = esc_html__( 'Comment Page %s', 'estore' ); // Text 'Comment Page N'.
 
-		$wrap_before = '<div class="breadcrumbs" itemscope itemtype="http://schema.org/BreadcrumbList">'; // the opening wrapper tag
-		$wrap_after  = '</div><!-- .breadcrumbs -->'; // the closing wrapper tag
-		$sep         = '&nbsp;&frasl;&nbsp;'; // separator between crumbs
-		$before      = '<span class="current">'; // tag before the current crumb
-		$after       = '</span>'; // tag after the current crumb
+		$wrap_before = '<div class="breadcrumbs" itemscope itemtype="http://schema.org/BreadcrumbList">'; // The opening wrapper tag.
+		$wrap_after  = '</div><!-- .breadcrumbs -->'; // The closing wrapper tag.
+		$sep         = '&nbsp;&frasl;&nbsp;'; // Separator between crumbs.
+		$before      = '<span class="current">'; // Tag before the current crumb.
+		$after       = '</span>'; // Tag after the current crumb.
 
-		$show_on_home   = 1; // 1 - show breadcrumbs on the homepage, 0 - don't show
-		$show_home_link = 1; // 1 - show the 'Home' link, 0 - don't show
-		$show_current   = 1; // 1 - show current page title, 0 - don't show
-		$show_last_sep  = 1; // 1 - show last separator, when current page title is not displayed, 0 - don't show
-		/* === END OF OPTIONS === */
+		$show_on_home   = 1; // 1 - Show breadcrumbs on the homepage, 0 - don't show.
+		$show_home_link = 1; // 1 - Show the 'Home' link, 0 - don't show.
+		$show_current   = 1; // 1 - Show current page title, 0 - don't show.
+		$show_last_sep  = 1; // 1 - Show last separator, when current page title is not displayed, 0 - don't show.
+		// End of options.
 
 		global $post;
 		$home_url  = esc_url( home_url() ) . '/';
@@ -129,103 +129,137 @@ if ( ! function_exists( 'estore_breadcrumbs' ) ) :
 
 		if ( is_home() || is_front_page() ) {
 
-			if ( $show_on_home == 1 ) {
+			if ( 1 === $show_on_home ) {
 				echo $wrap_before . $home_link . $wrap_after;
 			}
 		} else {
-
 			$position = 0;
 
 			echo $wrap_before;
 
 			if ( $show_home_link ) {
 				$position += 1;
+
 				echo $home_link;
 			}
 
 			if ( is_category() ) {
 				$parents = get_ancestors( get_query_var( 'cat' ), 'category' );
+
 				foreach ( array_reverse( $parents ) as $cat ) {
 					$position += 1;
+
 					if ( $position > 1 ) {
 						echo $sep;
 					}
+
 					echo sprintf( $link, get_category_link( $cat ), get_cat_name( $cat ), $position );
 				}
+
 				if ( get_query_var( 'paged' ) ) {
 					$position += 1;
-					$cat       = get_query_var( 'cat' );
+
+					$cat = get_query_var( 'cat' );
+
 					echo $sep . sprintf( $link, get_category_link( $cat ), get_cat_name( $cat ), $position );
+
 					echo $sep . $before . sprintf( $text['page'], get_query_var( 'paged' ) ) . $after;
 				} else {
+
 					if ( $show_current ) {
+
 						if ( $position >= 1 ) {
 							echo $sep;
 						}
+
 						echo $before . sprintf( $text['category'], single_cat_title( '', false ) ) . $after;
 					} elseif ( $show_last_sep ) {
 						echo $sep;
 					}
 				}
 			} elseif ( is_search() ) {
+
 				if ( get_query_var( 'paged' ) ) {
 					$position += 1;
+
 					if ( $show_home_link ) {
 						echo $sep;
 					}
+
 					echo sprintf( $link, $home_url . '?s=' . get_search_query(), sprintf( $text['search'], get_search_query() ), $position );
+
 					echo $sep . $before . sprintf( $text['page'], get_query_var( 'paged' ) ) . $after;
 				} else {
+
 					if ( $show_current ) {
+
 						if ( $position >= 1 ) {
 							echo $sep;
 						}
+
 						echo $before . sprintf( $text['search'], get_search_query() ) . $after;
 					} elseif ( $show_last_sep ) {
 						echo $sep;
 					}
 				}
-			} elseif ( is_year() ) {
-				if ( $show_home_link && $show_current ) {
-					echo $sep;
-				}
-				if ( $show_current ) {
-					echo $before . get_the_time( 'Y' ) . $after;
-				} elseif ( $show_home_link && $show_last_sep ) {
-					echo $sep;
-				}
-			} elseif ( is_month() ) {
-				if ( $show_home_link ) {
-					echo $sep;
-				}
-				$position += 1;
-				echo sprintf( $link, get_year_link( get_the_time( 'Y' ) ), get_the_time( 'Y' ), $position );
-				if ( $show_current ) {
-					echo $sep . $before . get_the_time( 'F' ) . $after;
-				} elseif ( $show_last_sep ) {
-					echo $sep;
-				}
 			} elseif ( is_day() ) {
+
 				if ( $show_home_link ) {
 					echo $sep;
 				}
+
 				$position += 1;
+
 				echo sprintf( $link, get_year_link( get_the_time( 'Y' ) ), get_the_time( 'Y' ), $position ) . $sep;
+
 				$position += 1;
+
 				echo sprintf( $link, get_month_link( get_the_time( 'Y' ), get_the_time( 'm' ) ), get_the_time( 'F' ), $position );
+
 				if ( $show_current ) {
 					echo $sep . $before . get_the_time( 'd' ) . $after;
 				} elseif ( $show_last_sep ) {
 					echo $sep;
 				}
+			} elseif ( is_month() ) {
+
+				if ( $show_home_link ) {
+					echo $sep;
+				}
+
+				$position += 1;
+
+				echo sprintf( $link, get_year_link( get_the_time( 'Y' ) ), get_the_time( 'Y' ), $position );
+
+				if ( $show_current ) {
+					echo $sep . $before . get_the_time( 'F' ) . $after;
+				} elseif ( $show_last_sep ) {
+					echo $sep;
+				}
+			} elseif ( is_year() ) {
+
+				if ( $show_home_link && $show_current ) {
+					echo $sep;
+				}
+
+				if ( $show_current ) {
+					echo $before . get_the_time( 'Y' ) . $after;
+				} elseif ( $show_home_link && $show_last_sep ) {
+					echo $sep;
+				}
 			} elseif ( is_single() && ! is_attachment() ) {
+
 				if ( get_post_type() != 'post' ) {
 					$position += 1;
+
 					$post_type = get_post_type_object( get_post_type() );
+
 					if ( $position > 1 ) {
 						echo $sep;
 					}
+
 					echo sprintf( $link, get_post_type_archive_link( $post_type->name ), $post_type->labels->name, $position );
+
 					if ( $show_current ) {
 						echo $sep . $before . get_the_title() . $after;
 					} elseif ( $show_last_sep ) {
@@ -237,18 +271,25 @@ if ( ! function_exists( 'estore_breadcrumbs' ) ) :
 					$parents   = get_ancestors( $catID, 'category' );
 					$parents   = array_reverse( $parents );
 					$parents[] = $catID;
+
 					foreach ( $parents as $cat ) {
 						$position += 1;
+
 						if ( $position > 1 ) {
 							echo $sep;
 						}
+
 						echo sprintf( $link, get_category_link( $cat ), get_cat_name( $cat ), $position );
 					}
+
 					if ( get_query_var( 'cpage' ) ) {
 						$position += 1;
+
 						echo $sep . sprintf( $link, get_permalink(), get_the_title(), $position );
+
 						echo $sep . $before . sprintf( $text['cpage'], get_query_var( 'cpage' ) ) . $after;
 					} else {
+
 						if ( $show_current ) {
 							echo $sep . $before . get_the_title() . $after;
 						} elseif ( $show_last_sep ) {
@@ -258,17 +299,23 @@ if ( ! function_exists( 'estore_breadcrumbs' ) ) :
 				}
 			} elseif ( is_post_type_archive() ) {
 				$post_type = get_post_type_object( get_post_type() );
+
 				if ( get_query_var( 'paged' ) ) {
 					$position += 1;
+
 					if ( $position > 1 ) {
 						echo $sep;
 					}
+
 					echo sprintf( $link, get_post_type_archive_link( $post_type->name ), $post_type->label, $position );
+
 					echo $sep . $before . sprintf( $text['page'], get_query_var( 'paged' ) ) . $after;
 				} else {
+
 					if ( $show_home_link && $show_current ) {
 						echo $sep;
 					}
+
 					if ( $show_current ) {
 						echo $before . $post_type->label . $after;
 					} elseif ( $show_home_link && $show_last_sep ) {
@@ -282,24 +329,32 @@ if ( ! function_exists( 'estore_breadcrumbs' ) ) :
 				$parents   = get_ancestors( $catID, 'category' );
 				$parents   = array_reverse( $parents );
 				$parents[] = $catID;
+
 				foreach ( $parents as $cat ) {
 					$position += 1;
+
 					if ( $position > 1 ) {
 						echo $sep;
 					}
+
 					echo sprintf( $link, get_category_link( $cat ), get_cat_name( $cat ), $position );
 				}
+
 				$position += 1;
+
 				echo $sep . sprintf( $link, get_permalink( $parent ), $parent->post_title, $position );
+
 				if ( $show_current ) {
 					echo $sep . $before . get_the_title() . $after;
 				} elseif ( $show_last_sep ) {
 					echo $sep;
 				}
 			} elseif ( is_page() && ! $parent_id ) {
+
 				if ( $show_home_link && $show_current ) {
 					echo $sep;
 				}
+
 				if ( $show_current ) {
 					echo $before . get_the_title() . $after;
 				} elseif ( $show_home_link && $show_last_sep ) {
@@ -307,28 +362,38 @@ if ( ! function_exists( 'estore_breadcrumbs' ) ) :
 				}
 			} elseif ( is_page() && $parent_id ) {
 				$parents = get_post_ancestors( get_the_ID() );
+
 				foreach ( array_reverse( $parents ) as $pageID ) {
 					$position += 1;
+
 					if ( $position > 1 ) {
 						echo $sep;
 					}
+
 					echo sprintf( $link, get_page_link( $pageID ), get_the_title( $pageID ), $position );
 				}
+
 				if ( $show_current ) {
 					echo $sep . $before . get_the_title() . $after;
 				} elseif ( $show_last_sep ) {
 					echo $sep;
 				}
 			} elseif ( is_tag() ) {
+
 				if ( get_query_var( 'paged' ) ) {
 					$position += 1;
-					$tagID     = get_query_var( 'tag_id' );
+
+					$tagID = get_query_var( 'tag_id' );
+
 					echo $sep . sprintf( $link, get_tag_link( $tagID ), single_tag_title( '', false ), $position );
+
 					echo $sep . $before . sprintf( $text['page'], get_query_var( 'paged' ) ) . $after;
 				} else {
+
 					if ( $show_home_link && $show_current ) {
 						echo $sep;
 					}
+
 					if ( $show_current ) {
 						echo $before . sprintf( $text['tag'], single_tag_title( '', false ) ) . $after;
 					} elseif ( $show_home_link && $show_last_sep ) {
@@ -337,14 +402,19 @@ if ( ! function_exists( 'estore_breadcrumbs' ) ) :
 				}
 			} elseif ( is_author() ) {
 				$author = get_userdata( get_query_var( 'author' ) );
+
 				if ( get_query_var( 'paged' ) ) {
 					$position += 1;
+
 					echo $sep . sprintf( $link, get_author_posts_url( $author->ID ), sprintf( $text['author'], $author->display_name ), $position );
+
 					echo $sep . $before . sprintf( $text['page'], get_query_var( 'paged' ) ) . $after;
 				} else {
+
 					if ( $show_home_link && $show_current ) {
 						echo $sep;
 					}
+
 					if ( $show_current ) {
 						echo $before . sprintf( $text['author'], $author->display_name ) . $after;
 					} elseif ( $show_home_link && $show_last_sep ) {
@@ -352,18 +422,22 @@ if ( ! function_exists( 'estore_breadcrumbs' ) ) :
 					}
 				}
 			} elseif ( is_404() ) {
+
 				if ( $show_home_link && $show_current ) {
 					echo $sep;
 				}
+
 				if ( $show_current ) {
 					echo $before . $text['404'] . $after;
 				} elseif ( $show_last_sep ) {
 					echo $sep;
 				}
 			} elseif ( has_post_format() && ! is_singular() ) {
+
 				if ( $show_home_link && $show_current ) {
 					echo $sep;
 				}
+
 				echo get_post_format_string( get_post_format() );
 			}
 
