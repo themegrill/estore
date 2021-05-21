0c25b905e3f41d07ec0a5733fdcd8899b7532d32
ankitkarki17 <ankitkarki17@gmail.com>
2021-05-21 10:00:14 +0545

    Fix - PHP notice error on TG: Logo widget

--- inc/widgets/classs-estore-logo-widget.php
+++ inc/widgets/classs-estore-logo-widget.php
@@ -88,7 +88,7 @@ class estore_logo_widget extends WP_Widget {
 		$instance          = $old_instance;
 		$instance['title'] = sanitize_text_field( $new_instance['title'] );
 
-		for ( $i = 1; $i < 7; $i ++ ) {
+		for ( $i = 1; $i < 6; $i ++ ) {
 			$image_link = 'logo_link' . $i;
 			$image_url  = 'logo_image' . $i;
 
