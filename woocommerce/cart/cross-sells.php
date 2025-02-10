04f6a2072a6c44673fb42ca08e8cd0032fba9ff2
som shrestha <109511507+som3669@users.noreply.github.com>
2025-02-10 16:34:29 +0545

    fix template ovrided issue (#135)

--- woocommerce/cart/cross-sells.php
+++ woocommerce/cart/cross-sells.php
@@ -13,7 +13,7 @@
  * @see         https://docs.woocommerce.com/document/template-structure/
  * @author      WooThemes
  * @package     WooCommerce\Templates
- * @version     4.4.0
+ * @version     9.6.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
